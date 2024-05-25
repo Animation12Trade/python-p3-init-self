741efa382c72ead8074d9bec223a16543f40ab23
+ <omuhindaaaron@gmail.com>
2024-05-25 15:17:45 +0300

    Enter new data

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+# Creating instances of Dog
+fido = Dog(name="Fido", breed="Dalmatian")
+buddy = Dog(name="Buddy")  
+
+# Calling instance methods
+fido.bark()
+fido.sit()
+buddy.bark()
+buddy.sit()
+
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+# Creating an instance of Person
+john = Person(name="John")
+
+# Calling instance methods
+john.talk()
+john.walk()
